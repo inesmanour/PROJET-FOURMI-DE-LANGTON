9097b84f9e7077318f702da2c8d163062350eca8
Laura LEFEVRE <laura.lefevre@ens.uvsq.fr>
2022-04-15 10:53:03 +0200

    Merge branch 'main' of https://github.com/uvsq22103405/PROJET-FOURMI-DE-LANGTON

--- projet_Fourmi.py
+++ projet_Fourmi.py
@@ -6,16 +6,11 @@
 # ADAM KEDDIS
 # MANOUR INES
 #
-
 #########################################
 # import des librairies
 
-from distutils.command.config import config
-from glob import glob
 from tkinter import *
 from random import *
-from functools import partial
-from winreg import REG_OPENED_EXISTING_KEY
 
 #########################################
 # constantes
@@ -24,80 +19,12 @@ from winreg import REG_OPENED_EXISTING_KEY
 HAUTEUR = 600
 # largeur du canevas
 LARGEUR = 600
-# boucle avalanche
-boucle = False
+
 #########################################
 # variables globales
 
-# Cases de la grille
-cases = []
-# constante de proportionnalité
-n = 50
-# configuration du canevas stockée dans une liste a deux dimensions
-config_courante = []
-
-
 #########################################
 # fonctions
-def fourmi_de_langton(n):
-    '''Fonction intermédiaire pour lancer le programme tas de sable'''
-    configuration_courante_vide(n)
-
-
-
-
-def configuration_courante_vide(n):
-    '''Initialise la configuration conrante vide'''
-    global config_courante
-    global cases
-
-    for i in range(n):
-        config_courante.append([0 for x in range(n)])
-
-    grille(n)
-
-
-def grille(n):
-    '''Fonctions qui affiche la grille '''
-    c = HAUTEUR / n
-    global cases
-    for ligne in range(n):
-        transit = []
-        for colonne in range(n):
-            transit.append(
-                canevas.create_rectangle(colonne * c + 2, ligne * c + 2, (colonne + 1) * c + 2, (ligne + 1) * c + 2))
-        cases.append(transit)
-
-    maj_grille(n)
-
-
-def maj_grille(n):
-    '''Fonction qui met à jour la grille en fonction de la configuration courante'''
-    for ligne in range(n):
-        for colonne in range(n):
-            if config_courante[ligne][colonne] == 0:
-                canevas.itemconfigure(cases[ligne][colonne], fill='black', outline='grey')  # noir pour 0 grain
-
-
-            elif config_courante[ligne][colonne] == 1:
-                canevas.itemconfigure(cases[ligne][colonne], fill='white', outline='grey')  # blanc pour 1 grain
-
-
-
-
-def random_config(n):
-    ''' Fonction qui initialise une configuration aléatoire : ajoute entre 0 et n grains de sable à chaque case'''
-    global config_courante
-    for ligne in range(n):
-        for colonne in range(n):
-            config_courante[ligne][colonne] = randint(0, 1)  # il faut écraser la variable et pas l'additionner !!!
-    maj_grille(n)
-
-
-
-
-
-
 
 
 #########################################
@@ -108,15 +35,12 @@ def random_config(n):
 fenetre = Tk()
 fenetre.title("Projet 1 : Fourmi de Langton")
 canevas = Canvas(fenetre, height=HAUTEUR, width=LARGEUR, bg="snow")
-boutton_random_config = Button(fenetre, text="Configuration aléatoire", width=17, height=3, bg="moccasin",
-                               fg="black", command=partial(random_config, n))
 
 
 # placement des widgets
 canevas.grid(column=1, row=0, columnspan=2, rowspan=5, padx=3, pady=3)
-boutton_random_config.grid(column=0, row=0, )
 
 
 # boucle principale
-fourmi_de_langton(n)
+
 fenetre.mainloop()
